b5fb50d0a46cc4cf981d2cb6a663d4bd13a6878b
antu09 <antumaster09@hotmail.com>
2022-03-11 21:28:41 -0300

    ejercicio calculadora

---
+++ holaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace contador
+{ 
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int contador = 0, suma = 0;
+      while(contador < 10)
+      {
+        suma += contador;   //igual a suma = suma + contador
+        contador++;   //igual a contador + 1
+        Console.WriteLine(suma);
+      }
+
+      Console.WriteLine("La suma final es: " + suma);
+    }
+  }
+}
